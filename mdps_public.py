5329107da8228e78d1eef3e26ee8327835639e43
Divyansh Jain <88320999+divyanshgwl@users.noreply.github.com>
2022-10-08 00:03:50 +0530

    Update mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -1,9 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Sun May  8 21:01:15 2022
-
-@author: siddhardhan
-"""
 
 import pickle
 import streamlit as st
@@ -12,11 +7,11 @@ from streamlit_option_menu import option_menu
 
 # loading the saved models
 
-diabetes_model = pickle.load(open('diabetes_model.sav', 'rb'))
+
 
 heart_disease_model = pickle.load(open('heart_disease_model.sav', 'rb'))
 
-parkinsons_model = pickle.load(open('parkinsons_model.sav', 'rb'))
+
 
 
 
